0e515df41db943095b1472eea61fb8dda2272294
IHAR STALIAROU <ihar.staliarou.dev@gmail.com>
2026-01-13 16:25:52 +0300

    feat: added base schemas for user and pet

---
+++ lib/prisma.ts
@@ -0,0 +1,14 @@
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client/extension'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasource: {
+      url: process.env.DATABASE_URL,
+    },
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
