9d76d73a1ad80485aa75b63e28b8afd149abbba4
IHAR STALIAROU <ihar.staliarou.dev@gmail.com>
2026-01-21 15:20:15 +0300

    feat: added mobile nav menu

--- components/layout/Header/HeaderClient.tsx
+++ components/layout/Header/HeaderClient.tsx
@@ -1,22 +1,19 @@
 'use client'
 
-import { AuthActionsBlock } from '@/components/AuthActionsBlock'
-import { Logo } from '@/components/Logo/Logo'
 import { getLinks } from '@/constants/navigation'
-import { Link } from '@/i18n/routing'
 import { IUser } from '@/interfaces/user'
 import { useUIStore } from '@/store/use-ui-store'
-import clsx from 'clsx'
 import { useTranslations } from 'next-intl'
 
-import { BurgerButton } from './BurgerButton'
+import { DesktopNavigation } from './DesktopNavMenu'
+import { MobileNavigation } from './MobileNavMenu'
 
 interface IHeaderClientProps {
   user: IUser | null
 }
 
 export const HeaderClient = ({ user }: IHeaderClientProps) => {
-  const { isMobileMenuOpen, toggleMobileMenu, closeMobileMenu } = useUIStore()
+  const { isMobileMenuOpen } = useUIStore()
 
   const isAuthenticated = !!user
   const links = getLinks(isAuthenticated)
@@ -25,47 +22,23 @@ export const HeaderClient = ({ user }: IHeaderClientProps) => {
 
   return (
     <header
-      className={clsx(
-        'fixed top-0 left-0 z-50 flex w-full flex-col border-b bg-white transition-all',
-        isMobileMenuOpen ? 'h-full md:h-20' : 'h-16 md:h-20'
-      )}
+      className={
+        'fixed top-0 left-0 z-50 flex h-16 w-full flex-col border-b bg-white transition-all md:h-20'
+      }
     >
-      {/* TODO: add fast access links to pets and profile (refactoring!?) */}
-      <nav
-        className={clsx(
-          'inset-0 z-40 flex h-full flex-col p-6 pt-10 transition-transform duration-300 ' +
-            'justify-between md:static md:h-full md:max-w-360 md:flex-row md:px-4 md:py-0 2xl:mx-auto 2xl:w-360',
-          isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
-        )}
-      >
-        <Logo closeMobileMenu={closeMobileMenu} />
+      <DesktopNavigation
+        user={user}
+        isAuthenticated={isAuthenticated}
+        links={links}
+        t={t}
+      />
 
-        <ul className='flex flex-col gap-6 md:flex-row md:items-center md:gap-8'>
-          {links.map(({ label, href }) => (
-            <li key={href}>
-              <Link
-                href={href}
-                onClick={closeMobileMenu}
-                className='hover:text-primary-hover font-medium transition-colors'
-              >
-                {t(label.replace('navigation.', ''))}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <AuthActionsBlock
-          user={user}
-          isAuthenticated={isAuthenticated}
-          closeMobileMenu={closeMobileMenu}
-        />
-        {/* TODO: add language  */}
-      </nav>
-
-      <BurgerButton
-        className='absolute top-3 right-5 z-50 md:hidden'
+      <MobileNavigation
+        user={user}
+        isAuthenticated={isAuthenticated}
+        links={links}
+        t={t}
         isMobileMenuOpen={isMobileMenuOpen}
-        toggleMobileMenu={toggleMobileMenu}
       />
     </header>
   )
