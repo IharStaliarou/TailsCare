3e262c47fa28b3d49aab6c9aa816970abe756af9
IHAR STALIAROU <ihar.staliarou.dev@gmail.com>
2026-01-30 21:02:54 +0300

    refactor: mobile sidebar styles and view

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -20,3 +20,14 @@ export interface IPet {
   createdAt: Date
   updatedAt: Date
 }
+
+export const MOCK_USER: IUser = {
+  id: 'wqe123we-qweqwe123-qweqwe123-ewwqe213',
+  mail: 'example@gmail.com',
+  phone: '375291231233',
+  name: 'John Doe',
+  avatarUrl: '',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  pets: [],
+}
